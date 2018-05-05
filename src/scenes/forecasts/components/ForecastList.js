39e5c2c3c935e95a5eeb173f0f5b3e032d0c10aa
Philip Grin <pgrin@philbookpromini.local>
2018-05-05 12:02:47 +0200

    moved locations to config

--- src/scenes/forecasts/components/ForecastList.js
+++ src/scenes/forecasts/components/ForecastList.js
@@ -2,47 +2,19 @@ import React, { Component } from 'react';
 import './ForecastList.css';
 
 // Semantic UI Components
-import { Segment } from 'semantic-ui-react'
+import { Segment } from 'semantic-ui-react';
 
 // Components
 import Forecast from './Forecast';
+
+const config = require('../../../config/config.json');
 
 class ForecastList extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      // TODO: Extract this
-      locations: [
-        {
-          name: 'Crystal Mountain',
-          point: {
-            lat: '46.9388',
-            lon: '-121.4731',
-          }
-        },
-        {
-          name: 'Stevens Pass',
-          point: {
-            lat: '47.7456',
-            lon: '-121.0892',
-          }
-        },
-        {
-          name: 'Mt Baker',
-          point: {
-            lat: '48.858',
-            lon: '-121.6697',
-          }
-        },
-        {
-          name: 'Snoqualmie 👎🏻',
-          point: {
-            lat: '47.4587',
-            lon: '-121.4165',
-          },
-        },
-      ]
+      locations: config.locations
     };
   }
 
